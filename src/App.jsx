7ab840e13435b1d122fad94d0615dbaf2fa916f9
Watsuphol <wasumc.design@gmail.com>
2024-06-26 23:46:29 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarAdmin from "./components/Navbar/NavbarAdmin";
+import Admin from "./page/admin/Admin";
+
+const App = () => {
+  return (
+    <div>
+      <NavbarAdmin />
+      <Admin />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/page/admin/Admin.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import AddProduct from "../../components/AddProduct/AddProduct";
+import ListProduct from "../../components/ListProduct/ListProduct";
+import OrderManagement from "../../components/OrderManagement/OrderManagement";
+import Customers from "../../components/Customers/Customers";
+import Transaction from "../../components/Transaction/Transaction";
+
+const Admin = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Routes>
+        <Route path="/orderManagement" element={<OrderManagement />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/transaction" element={<Transaction />} />
+        <Route path="/addproduct" element={<AddProduct />} />
+        <Route path="/listproduct" element={<ListProduct />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Admin;
