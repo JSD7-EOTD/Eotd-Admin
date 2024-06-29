dc8c1c965768b7683f3415171ad99b9f8612b838
Watsuphol <wasumc.design@gmail.com>
2024-06-29 22:32:06 +0700

    Updated Finish AddProduct component

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -2,8 +2,58 @@ import React, { useState } from "react";
 import upload_area from "../../assets/upload_area.svg";
 
 const AddProduct = () => {
-  // const [image, setImage] = useState(false);
-  // const imageHandler = (e)
+  const [image, setImage] = useState(false);
+  const [productDetails, setProductDetails] = useState({
+    name: "",
+    image: "",
+    category: "Toys",
+    new_price: "",
+    old_price: "",
+  });
+  const imageHandler = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const changeHandler = (e) => {
+    setProductDetails({ ...productDetails, [e.target.name]: e.target.value });
+  };
+  const Add_Product = async () => {
+    console.log(productDetails);
+    let responseData;
+    let product = productDetails;
+
+    let formData = new FormData();
+    formData.append("product", image);
+
+    await fetch("http://localhost:4000/upload", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+      body: formData,
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        responseData = data;
+      });
+
+    if (responseData.success) {
+      product.image = responseData.image_url;
+      console.log(product);
+      await fetch("http://localhost:4000/", {
+        method: "Post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          data.success ? alert("Product Added") : alert("Failed");
+        });
+    }
+  };
 
   return (
     <div className="flex flex-col gap-5 w-full max-w-[800px] p-8 px-12 m-5 mx-7 rounded-md bg-white box-border">
@@ -11,6 +61,8 @@ const AddProduct = () => {
       <div className="flex flex-col gap-5 w-full text-gray-500 text-base">
         <p>Product title</p>
         <input
+          value={productDetails.name}
+          onChange={changeHandler}
           type="text"
           name="name"
           placeholder="Type here"
@@ -23,6 +75,8 @@ const AddProduct = () => {
         <div className="flex flex-col gap-5 w-full text-gray-500 text-base">
           <p>Price</p>
           <input
+            value={productDetails.old_price}
+            onChange={changeHandler}
             type="text"
             name="old_price"
             placeholder="Type here"
@@ -32,6 +86,8 @@ const AddProduct = () => {
         <div className="flex flex-col gap-5 w-full text-gray-500 text-base">
           <p>Offer Price</p>
           <input
+            value={productDetails.new_price}
+            onChange={changeHandler}
             type="text"
             name="new_price"
             placeholder="Type here"
@@ -44,6 +100,8 @@ const AddProduct = () => {
       <div className="w-full text-gray-500 text-base">
         <p>Product Category</p>
         <select
+          value={productDetails.category}
+          onChange={changeHandler}
           name="category"
           className="bg-white p-2 w-full max-w-[200px] h-[50px] text-[14px] text-[#7b7b7b] border border-[#7b7b7b8d] rounded-[4px]"
         >
@@ -58,16 +116,25 @@ const AddProduct = () => {
       <div className="flex flex-col items-start">
         <label htmlFor="file-input" className="cursor-pointer">
           <img
-            src={upload_area}
+            src={image ? URL.createObjectURL(image) : upload_area}
             alt="Upload Area"
             className="h-[120px] w-[120px] rounded-[10px] my-[10px] mx-0 object-contain"
           />
-          <input type="file" name="image" id="file-input" hidden />
+          <input
+            onChange={imageHandler}
+            type="file"
+            name="image"
+            id="file-input"
+            hidden
+          />
         </label>
       </div>
 
       {/* Add Button */}
-      <button className="mt-5 w-[160px] h-[50px] rounded-[6px] bg-[#bd8356] border-none cursor-pointer text-white text-[16px] font-medium relative overflow-hidden transition-transform duration-300 ease-out hover:scale-105 active:scale-95 focus:outline-none focus:ring-2 focus:ring-white">
+      <button
+        onClick={Add_Product}
+        className="mt-5 w-[160px] h-[50px] rounded-[6px] bg-[#bd8356] border-none cursor-pointer text-white text-[16px] font-medium relative overflow-hidden transition-transform duration-300 ease-out hover:scale-105 active:scale-95 focus:outline-none focus:ring-2 focus:ring-white"
+      >
         ADD
         <span className="absolute inset-0 bg-[#794222] opacity-0 transition-opacity duration-300 ease-out hover:opacity-30"></span>
       </button>
--- src/components/ListProduct/ListProduct.jsx
+++ src/components/ListProduct/ListProduct.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const ListProduct = () => {
-  return <div></div>;
+  const [allproducts, setAllproducts] = useState([]);
+  const fetchInfo = async () => {
+    await fetch("http://localhost:4000/allproducts")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllproducts(data);
+      });
+  };
+
+  return (
+    <div>
+      <h1 className="text-2xl text-black">All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>Product</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+      </div>
+    </div>
+  );
 };
 
 export default ListProduct;
